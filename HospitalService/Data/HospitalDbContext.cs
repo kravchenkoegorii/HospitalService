a55efb01cbf63b7036d32ad5c6d1ffd96188cb80
kravchenkoegorii <kravchenko13138@gmail.com>
2022-04-10 16:29:57 +0300

    add class HospitalDbContext

---
+++ HospitalService/Data/HospitalDbContext.cs
@@ -0,0 +1,12 @@
+﻿using HospitalService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalService.Data;
+
+public class HospitalDbContext:DbContext
+{
+    Doctor doctor = new Doctor
+    {
+        Id = 1, FirstName = "Egor", LastName = "Kravchenko", Age = 18, Specialization = "Pediatrist"
+    };
+}
--- HospitalService/Startup.cs
+++ HospitalService/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HospitalService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace HospitalService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<HospitalDbContext>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
