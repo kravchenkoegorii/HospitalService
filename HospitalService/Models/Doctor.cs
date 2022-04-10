4a6a792c77d44662a5edfb5d4a8e9b86b01eedea
kravchenkoegorii <kravchenko13138@gmail.com>
2022-04-10 15:22:54 +0300

    add class Doctor

---
+++ HospitalService/Models/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalService.Models;
+
+public class Doctor
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Specialization { get; set; }
+}
