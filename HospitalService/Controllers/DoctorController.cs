73e62364a5fd0554999b4ae0331610456b834ba3
kravchenkoegorii <kravchenko13138@gmail.com>
2022-04-15 21:01:05 +0300

    add changes

---
+++ HospitalService/Controllers/DoctorController.cs
@@ -0,0 +1,42 @@
+﻿using HospitalService.Data;
+using HospitalService.Models;
+using System.Collections.Generic;
+
+namespace HospitalService.Controllers
+{
+    public class DoctorController: IDoctorRepository
+    {
+        private HospitalDbContext DbContext { get; set; }
+        private List<Doctor> _doctors;
+
+        public DoctorController(HospitalDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public void ChangeDoctor(Doctor doctor, int id)
+        {
+            _doctors[id] = doctor;
+        }
+
+        public void CreateDoctor(Doctor doctor)
+        {
+            _doctors.Add(doctor);
+        }
+
+        public void DeleteDoctor(int id)
+        {
+            _doctors.RemoveAt(id);
+        }
+
+        public Doctor GetDoctor(int id)
+        {
+            return _doctors[id];
+        }
+
+        public IEnumerable<Doctor> GetDoctors()
+        {
+            return _doctors;
+        }
+    }
+}
---
+++ HospitalService/Data/IDoctorRepository.cs
@@ -0,0 +1,20 @@
+﻿using HospitalService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalService.Data
+{
+    public interface IDoctorRepository
+    {
+        public void CreateDoctor(Doctor doctor);
+
+        public void DeleteDoctor(int id);
+
+        public Doctor GetDoctor(int id);
+
+        public IEnumerable<Doctor> GetDoctors();
+
+        public void ChangeDoctor(Doctor doctor, int id);
+    }
+}
