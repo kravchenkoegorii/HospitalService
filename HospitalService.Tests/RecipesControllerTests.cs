01dcf093b8df091deca00a47ba84f57f09f51920
kravchenkoegorii <kravchenko13138@gmail.com>
2022-07-11 17:16:48 +0300

    add tests

--- HospitalService.Tests/RecipesControllerTests.cs
+++ HospitalService.Tests/RecipesControllerTests.cs
@@ -15,9 +15,6 @@ namespace HospitalService.Tests
         [Fact]
         public void GetPatients_Older_Than_18()
         {
-            /*var expected = Expected();
-
-            var patients = Actual();*/
             var patients = new List<Patient>
             {
                 new Patient(1, "Egorka", "Kravch", 16, "bbb", 1),
@@ -53,7 +50,7 @@ namespace HospitalService.Tests
         }//true test
 
         [Fact]
-        public void GetPatients_Younger_Than_18()//error test
+        public void GetPatients_Younger_Than_18() //error test
         {
             var patients = new List<Patient>
             {
@@ -87,31 +84,6 @@ namespace HospitalService.Tests
             // Assert
 
             Assert.Equal(expected, actual.Result);
-        }
-        public List<Patient> Expected()
-        {
-            var expected = new List<Patient>
-            {
-                new Patient(2, "Egorka", "Kravch", 21, "bbb", 1),
-                new Patient(4, "Egorka", "Kravch", 20, "bbb", 1),
-                new Patient(5, "Egorka", "Kravch", 100, "bbb", 1),
-                new Patient(6, "Egorka", "Kravch", 30, "bbb", 1)
-            };
-            return expected;
-        }
-
-        public List<Patient> Actual()
-        {
-            var actual = new List<Patient>
-            {
-                new Patient(1, "Egorka", "Kravch", 16, "bbb", 1),
-                new Patient(2, "Egorka", "Kravch", 21, "bbb", 1),
-                new Patient(3, "Egorka", "Kravch", 15, "bbb", 1),
-                new Patient(4, "Egorka", "Kravch", 20, "bbb", 1),
-                new Patient(5, "Egorka", "Kravch", 100, "bbb", 1),
-                new Patient(6, "Egorka", "Kravch", 30, "bbb", 1)
-            };
-            return actual;
         }
     }
 }
