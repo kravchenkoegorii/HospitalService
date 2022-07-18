6d3164b3d73494282262c173312817b23e9b6209
kravchenkoegorii <kravchenko13138@gmail.com>
2022-07-18 14:49:59 +0300

    new exception

---
+++ HospitalService/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HospitalService.Exceptions
+{
+    public class UnauthorizedException : Exception
+    {
+        public UnauthorizedException() { }
+        public UnauthorizedException(string message) : base(message) { }
+    }
+}
