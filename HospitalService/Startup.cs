7697400bb390d88cdbe282ba3fd7a3640e559f39
kravchenkoegorii <kravchenko13138@gmail.com>
2022-08-12 14:52:06 +0300

    delete comments

--- HospitalService/Startup.cs
+++ HospitalService/Startup.cs
@@ -22,7 +22,6 @@ namespace HospitalService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationServices();
@@ -55,7 +54,6 @@ namespace HospitalService
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
